51c4ca2a29c04c04b674c4cea58d055bd475d4c1
Saahil9720 <saahilvekariya20@hotmail.com>
2023-11-17 20:33:47 +0300

    First Commit

---
+++ gitprofile.config.js
@@ -0,0 +1,134 @@
+// gitprofile.config.js
+
+const config = {
+  github: {
+    username: 'Saahil9720', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 10, // How many projects to display.
+    exclude: {
+      forks: false, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    linkedin: 'saahil-vekariya-8bb615239',
+    email: 'saahilvekariya20@hotamil.com',
+  },
+  resume: {
+    fileUrl:
+      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'JavaScript',
+    'React.js',
+    'Node.js',
+    'MySQL',
+    'Git',
+    'CSS',
+    'Python',
+    'Java',
+    'C#',
+    'Android',
+    'Design',
+    'CSS',
+  ],
+  experiences: [
+    {
+      company: 'Coauto Dealers Ltd',
+      position: 'Intern',
+      from: 'July 2019',
+      to: 'July 2019',
+      companyLink: 'https://example.com',
+    },
+    {
+      company: 'Educate Your Self Ltd',
+      position: 'Data Clerk',
+      from: 'May 2020',
+      to: 'December 2020',
+      companyLink: 'https://example.com',
+    },
+  ],
+ 
+  
+
+  // To hide the `My Projects` section, keep it empty.
+  externalProjects: [
+    // {
+    //   title: 'Project Name',
+    //   description:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //   imageUrl: 'https://via.placeholder.com/250x250',
+    //   link: 'https://example.com',
+    // },
+    // {
+    //   title: 'Project Name',
+    //   description:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //   imageUrl: 'https://via.placeholder.com/250x250',
+    //   link: 'https://example.com',
+    // },
+  ],
+  themeConfig: {
+    defaultTheme: 'wireframe',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  footer: `Made with ❤️`,
+};
+
+export default config;
---
+++ src/components/details/index.jsx
@@ -0,0 +1,153 @@
+import { MdLocationOn } from 'react-icons/md';
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillMediumSquare,
+} from 'react-icons/ai';
+import { SiTwitter } from 'react-icons/si';
+import { CgDribbble } from 'react-icons/cg';
+import { RiPhoneFill, RiMailFill } from 'react-icons/ri';
+import { Fragment } from 'react';
+import {
+  FaBuilding,
+  FaLinkedin,
+} from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { skeleton } from '../../helpers/utils';
+
+const isCompanyMention = (company) => {
+  return company.startsWith('@') && !company.includes(' ');
+};
+
+const companyLink = (company) => {
+  return `https://github.com/${company.substring(1)}`;
+};
+
+const getFormattedMastodonValue = (mastodonValue, isLink) => {
+  const [username, server] = mastodonValue.split('@');
+
+  if (isLink) {
+    return `https://${server}/@${username}`;
+  } else {
+    return `${username}@${server}`;
+  }
+};
+
+const ListItem = ({ icon, title, value, link, skeleton = false }) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      className="flex justify-start py-2 px-1 items-center"
+    >
+      <div className="flex-grow font-medium gap-2 flex items-center my-1">
+        {icon} {title}
+      </div>
+      <div
+        className={`${
+          skeleton ? 'flex-grow' : ''
+        } text-sm font-normal text-right mr-2 ml-3 ${link ? 'truncate' : ''}`}
+        style={{
+          wordBreak: 'break-word',
+        }}
+      >
+        {value}
+      </div>
+    </a>
+  );
+};
+
+const Details = ({ profile, loading, social, github }) => {
+  const renderSkeleton = () => {
+    let array = [];
+    for (let index = 0; index < 4; index++) {
+      array.push(
+        <ListItem
+          key={index}
+          skeleton={true}
+          icon={skeleton({ width: 'w-4', height: 'h-4' })}
+          title={skeleton({ width: 'w-24', height: 'h-4' })}
+          value={skeleton({ width: 'w-full', height: 'h-4' })}
+        />
+      );
+    }
+
+    return array;
+  };
+
+  return (
+    <div className="card shadow-lg compact bg-base-100">
+      <div className="card-body">
+        <div className="text-base-content text-opacity-60">
+          {loading || !profile ? (
+            renderSkeleton()
+          ) : (
+            <Fragment>
+              {profile.location && (
+                <ListItem
+                  icon={<MdLocationOn />}
+                  title="Based in:"
+                  value={profile.location}
+                />
+              )}
+              {profile.company && (
+                <ListItem
+                  icon={<FaBuilding />}
+                  title="Company:"
+                  value={profile.company}
+                  link={
+                    isCompanyMention(profile.company.trim())
+                      ? companyLink(profile.company.trim())
+                      : null
+                  }
+                />
+              )}
+              <ListItem
+                icon={<AiFillGithub />}
+                title="GitHub:"
+                value={github.username}
+                link={`https://github.com/${github.username}`}
+              />
+             
+              {social?.linkedin && (
+                <ListItem
+                  icon={<FaLinkedin />}
+                  title="LinkedIn:"
+                  value={social.linkedin}
+                  link={`https://www.linkedin.com/in/${social.linkedin}`}
+                />
+              )}
+              
+              {social?.email && (
+                <ListItem
+                  icon={<RiMailFill />}
+                  title="Email:"
+                  value={social.email}
+                  link={`mailto:${social.email}`}
+                />
+              )}
+            </Fragment>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Details.propTypes = {
+  profile: PropTypes.object,
+  loading: PropTypes.bool.isRequired,
+  social: PropTypes.object.isRequired,
+  github: PropTypes.object.isRequired,
+};
+
+ListItem.propTypes = {
+  icon: PropTypes.node,
+  title: PropTypes.node,
+  value: PropTypes.node,
+  link: PropTypes.string,
+  skeleton: PropTypes.bool,
+};
+
+export default Details;
